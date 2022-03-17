398a100a377954752288191aa994ed31cdbb4241
Bruno Cruz <bru.cruz@gmail.com>
2022-03-16 21:34:02 -0300

    feat: structured project files based on vue's project version

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Logo } from './assets/cm-logo.svg';
+import { PremiumRecipeCard } from './components/PremiumRecipeCard/PremiumRecipeCard.comp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+    <div className="cm-container">
+      <div className="cm-logo-wrapper">
+        <Logo className="cm-logo"/>
+      </div>
+      <h2>Carb Manager Dev Assignment</h2>
+      <p>See the README file for assignment requirements.</p>
+
+      <ul>
+        <li className="premium-recipe">
+          <PremiumRecipeCard />
+        </li>
+      </ul>
     </div>
+  </div>
   );
 }
 
---
+++ src/components/PremiumRecipeCard/PremiumRecipeCard.comp.tsx
@@ -0,0 +1,11 @@
+export interface PremiumRecipeCardProps {
+  
+}
+
+export function PremiumRecipeCard({  }: PremiumRecipeCardProps): JSX.Element {
+  return (
+    <div>
+      PremiumRecipeCard
+    </div>
+  );
+}
