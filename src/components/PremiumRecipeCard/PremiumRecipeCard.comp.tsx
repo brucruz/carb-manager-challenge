8741ba18a3e6722c0e927aced95d160f7ec8086a
Bruno Cruz <bru.cruz@gmail.com>
2022-03-17 23:16:39 -0300

    feat: finished styling PremiumRecipeCard component

--- src/components/PremiumRecipeCard/PremiumRecipeCard.comp.tsx
+++ src/components/PremiumRecipeCard/PremiumRecipeCard.comp.tsx
@@ -1,31 +1,77 @@
 import * as Styles from './PremiumRecipeCard.styles';
 import food from '../../assets/food.png';
 import { ReactComponent as Trophy } from '../../assets/trophy.svg';
-import { ReactComponent as HeartEmpty } from '../../assets/heart-empty.svg';
+import { ReactComponent as Heart } from '../../assets/heart.svg';
+import { ReactComponent as Clock } from '../../assets/clock.svg';
+import { ReactComponent as Cals } from '../../assets/cals.svg';
+import { StarRating } from '../StarRating/StarRating.comp';
 
 export function PremiumRecipeCard(): JSX.Element {
   return (
     <Styles.PremiumRecipeCardContainer>
       <Styles.PremiumRecipeCardHeader>
-          <img src={food} alt="food" />
+        <img src={food} alt="food" />
 
-          <Styles.PremiumRecipeCardHeaderOverlay />
+        <Styles.PremiumRecipeCardHeaderOverlay />
 
-          <Styles.PremiumRecipeCardFavButton>
-            <HeartEmpty />
-          </Styles.PremiumRecipeCardFavButton>
+        <Styles.PremiumRecipeCardHeaderHoverOverlay />
 
-          <Styles.PremiumRecipeCardPremiumTag>
-            <Trophy />
-            <span>Premium Recipe</span>
-          </Styles.PremiumRecipeCardPremiumTag>
+        <Styles.PremiumRecipeCardFavButton>
+          <Heart />
+        </Styles.PremiumRecipeCardFavButton>
 
+        <Styles.PremiumRecipeCardPremiumTag>
+          <Trophy />
+          <span>Premium Recipe</span>
+        </Styles.PremiumRecipeCardPremiumTag>
       </Styles.PremiumRecipeCardHeader>
       <Styles.PremiumRecibeCardBottom>
         <Styles.PremiumRecipeCardTitle>
           Low Carb Thai Chicken Curry With Coconut Cauliflower Rice
         </Styles.PremiumRecipeCardTitle>
 
+        <Styles.PremiumRecipeCardRatings>
+          <StarRating
+            activeStars={3}
+            totalStars={5}
+            starDimensions={{
+              width: '11.85px',
+              height: '12px',
+            }}
+          />
+
+          <span>200 ratings</span>
+        </Styles.PremiumRecipeCardRatings>
+        <Styles.PremiumRecipeCardDetails>
+          <Styles.PremiumRecipeCardTimeToMake>
+            <Clock />
+
+            <span>24 min</span>
+          </Styles.PremiumRecipeCardTimeToMake>
+
+          <Styles.PremiumRecipeCardCalories>
+            <Cals />
+
+            <span>489 Calories</span>
+          </Styles.PremiumRecipeCardCalories>
+
+          <Styles.PremiumRecipeCardMacros>
+            <Styles.PremiumRecipeCardMacro macro="carbs">
+              <div />
+              <span>20g</span>
+            </Styles.PremiumRecipeCardMacro>
+
+            <Styles.PremiumRecipeCardMacro macro="protein">
+              <div />
+              <span>16g</span>
+            </Styles.PremiumRecipeCardMacro>
+
+            <Styles.PremiumRecipeCardMacro macro="fats">
+              <div />
+              <span>6g</span>
+            </Styles.PremiumRecipeCardMacro>
+          </Styles.PremiumRecipeCardMacros>
+        </Styles.PremiumRecipeCardDetails>
       </Styles.PremiumRecibeCardBottom>
     </Styles.PremiumRecipeCardContainer>
   );
--- src/components/PremiumRecipeCard/PremiumRecipeCard.styles.ts
+++ src/components/PremiumRecipeCard/PremiumRecipeCard.styles.ts
@@ -1,7 +1,17 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+
+export const PremiumRecipeCardHeaderHoverOverlay = styled.div``;
+
+export const PremiumRecipeCardFavButton = styled.div`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  color: transparent;
+  stroke: #fff;
+`;
 
 export const PremiumRecipeCardContainer = styled.div`
-  background: #FFFFFF;
+  background: #ffffff;
   box-shadow: 0px 13px 35px rgba(0, 30, 47, 0.1);
   border-radius: 12px;
 
@@ -9,7 +19,24 @@ export const PremiumRecipeCardContainer = styled.div`
   flex-direction: column;
   overflow: hidden;
   width: 343px;
-  `;
+
+  cursor: pointer;
+
+  &:hover {
+    ${PremiumRecipeCardHeaderHoverOverlay} {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(255, 255, 255, 0.2);
+    }
+    ${PremiumRecipeCardFavButton} {
+      color: #1bc98e;
+      stroke: #1bc98e;
+    }
+  }
+`;
 
 export const PremiumRecipeCardHeader = styled.div`
   position: relative;
@@ -36,7 +63,7 @@ export const PremiumRecipeCardPremiumTag = styled.div`
   left: 8px;
   display: flex;
   align-items: center;
-  
+
   background: rgba(255, 255, 255, 0.3);
   border-radius: 10px;
 
@@ -58,12 +85,6 @@ export const PremiumRecipeCardPremiumTag = styled.div`
   }
 `;
 
-export const PremiumRecipeCardFavButton = styled.div`
-  position: absolute;
-  top: 8px;
-  right: 8px;
-`;
-
 export const PremiumRecibeCardBottom = styled.div`
   padding: 8px 16px 16px 16px;
 `;
@@ -72,17 +93,103 @@ export const PremiumRecipeCardTitle = styled.span`
   font-weight: 700;
   font-size: 18px;
   line-height: 20px;
-  /* or 111% */
-
-
-  color: #0C0C0A;
+  color: #0c0c0a;
 
   white-space: break-spaces;
   text-align: start;
   overflow: hidden;
-   text-overflow: ellipsis;
-   display: -webkit-box;
-   -webkit-line-clamp: 2;
-           line-clamp: 2; 
-   -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
+export const PremiumRecipeCardRatings = styled.div`
+  display: flex;
+  margin-top: 17px;
+
+  span {
+    margin-left: 8px;
+    margin-top: 1px;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 14px;
+    color: #1ca677;
+  }
+`;
+
+export const PremiumRecipeCardDetails = styled.div`
+  margin-top: 8px;
+  display: flex;
+  width: 100%;
+`;
+
+export const PremiumRecipeCardTimeToMake = styled.div`
+  display: flex;
+  align-items: center;
+
+  span {
+    font-size: 12px;
+    line-height: 14px;
+    color: #393c40;
+    margin-left: 8.33px;
+  }
+`;
+
+export const PremiumRecipeCardCalories = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 16px;
+  flex: 1 1 auto;
+
+  span {
+    font-size: 12px;
+    line-height: 14px;
+    color: #393c40;
+    margin-left: 8px;
+  }
+`;
+
+export const PremiumRecipeCardMacros = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const PremiumRecipeCardMacro = styled.div<{
+  macro: 'protein' | 'carbs' | 'fats';
+}>`
+  display: flex;
+  align-items: center;
+
+  div {
+    width: 6px;
+    height: 6px;
+    border-radius: 50%;
+    background-color: ${({ macro }) => {
+      switch (macro) {
+        case 'protein':
+          return '#3177BB';
+        case 'carbs':
+          return '#F94642;';
+        case 'fats':
+          return '#FDA120';
+        default:
+          return '#3177BB';
+      }
+    }};
+  }
+
+  span {
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+
+    color: #6f737a;
+    margin-left: 4px;
+  }
+
+  & + & {
+    margin-left: 8px;
+  }
 `;
